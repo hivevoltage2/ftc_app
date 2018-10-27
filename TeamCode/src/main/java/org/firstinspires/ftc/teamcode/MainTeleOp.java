aad5533a38610bf8d2938335b6df71c9c038d7cf
Max <maxdsokolov@outlook.com>
2018-10-27 11:02:45 -0400

    Revert "Revert "In this update, I have made some significant changes to the code. I also added some comments to it so you could understand each new line.""
    This reverts commit 6f88a48e4b27c3cc52ba8656922534dd06182258.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainTeleOp.java
@@ -36,7 +36,7 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 
-@TeleOp(name="MainTeleOp", group="Linear Opmode")
+@TeleOp(name="MainTeleOp", group="Linear OpMode")
 //@Disabled
 public class MainTeleOp extends LinearOpMode {
 
@@ -47,6 +47,8 @@ public class MainTeleOp extends LinearOpMode {
     private DcMotor backLeft;
     private DcMotor backRight;
     private DcMotor arm;
+
+    //Here I have initialized motors that we are going to use in the future. The naming is temporary, we are probably gonna change how we call them
     private DcMotor slide1;
     private DcMotor slide2;
     private DcMotor lift;
@@ -54,12 +56,19 @@ public class MainTeleOp extends LinearOpMode {
     double leftPower;
     double rightPower;
     double armPower;
+
+    //Power values of slide1, slide2, and lift. The naming is temporary, we are probably gonna change how we call them
     double slide1Power;
     double slide2Power;
     double liftPower;
 
-    double drive;
-    double turn;
+    //Power values that will determine in what direction the robot is gonna go. Everything is explained below
+    double yDirection;
+    double xDirection;
+
+    //Power value that will add more power to the motors, as I decided that we don't have to use the full power of the motors as the robot drives
+    //If we use full power of the motors we are gonna lose precision.
+    double extraPower;
 
     @Override
     public void runOpMode() {
@@ -70,61 +79,65 @@ public class MainTeleOp extends LinearOpMode {
         frontRight = hardwareMap.get(DcMotor.class, "frontRight");
         backLeft  = hardwareMap.get(DcMotor.class, "backLeft");
         backRight = hardwareMap.get(DcMotor.class, "backRight");
-        slide1 = hardwareMap.get(DcMotor.class, "slide1");
-        slide2 = hardwareMap.get(DcMotor.class, "slide2");
         arm = hardwareMap.get(DcMotor.class, "arm");
-
-        if(frontLeft.isBusy()) {
-            telemetry.addLine("frontLeft: Active");
-        }else{
-            telemetry.addLine("frontLeft: Offline");
-        }
-
-        if(frontRight.isBusy()) {
-            telemetry.addLine("frontRight: Active");
-        }else{
-            telemetry.addLine("frontRight: Offline");
-        }
-
-        if(backLeft.isBusy()) {
-            telemetry.addLine("backLeft: Active");
-        }else{
-            telemetry.addLine("backLeft: Offline");
-        }
-
-        if(backRight.isBusy()) {
-            telemetry.addLine("backRight: Active");
-        }else{
-            telemetry.addLine("backRight: Offline");
-        }
-
+        //slide1 = hardwareMap.get(DcMotor.class, "slide1");
+        //slide2 = hardwareMap.get(DcMotor.class, "slide2");
+        //lift = hardwareMap.get(DcMotor.class, "lift");
 
         frontLeft.setDirection(DcMotor.Direction.FORWARD);
         frontRight.setDirection(DcMotor.Direction.REVERSE);
         backLeft.setDirection(DcMotor.Direction.FORWARD);
         backRight.setDirection(DcMotor.Direction.REVERSE);
         arm.setDirection(DcMotor.Direction.FORWARD);
-        slide1.setDirection(DcMotor.Direction.FORWARD);
-        slide2.setDirection(DcMotor.Direction.FORWARD);
+        //slide1.setDirection(DcMotor.Direction.FORWARD);
+        //slide2.setDirection(DcMotor.Direction.FORWARD);
+        //lift.setDirection(DcMotor.Direction.FORWARD);
 
         waitForStart();
         runtime.reset();
 
         while (opModeIsActive()) {
 
-            double drive = -gamepad1.left_stick_y;
-            double turn = -gamepad1.right_stick_x;
+            //As we are going to use only one stick (right stick) to control the movement of the robot. These statements make this possible.
+            //yDirection stands for movement along the y-axis.
+            //xDirection stands for movement along the x-axis.
+            //As xDirection increases, leftPower increases
+            //As xDirection decreases, rightPower decreases
+            yDirection = -gamepad1.right_stick_y;
+            xDirection = -gamepad1.right_stick_x;
+            leftPower = Range.clip(yDirection - xDirection, -1.0, 1.0);
+            rightPower = Range.clip(yDirection + xDirection, -1.0, 1.0);
 
-            armPower  = gamepad1.right_trigger;
-            armPower = -gamepad1.left_trigger;
+            //Here, we say that whenever user presses the right trigger, give extraPower the max value (1.0)
+            extraPower = gamepad1.right_trigger;
 
-            leftPower = Range.clip(drive + turn, -1.0, 1.0);
-            leftPower = Range.clip(drive + turn, -1.0, 1.0);
+            armPower = -gamepad1.left_stick_y;
 
-            frontLeft.setPower(leftPower);
-            frontRight.setPower(rightPower);
-            backLeft.setPower(leftPower);
-            backRight.setPower(rightPower);
+            //In these series of control statements we tone down the power of the motors by half (*0.5) and then add the value of extraPower (0 or 1, depending on the user's actions with the right trigger.
+            //In this case we add extraPower to the leftPower value
+            if(leftPower > 0){
+                backLeft.setPower(0.5*leftPower + 0.5*extraPower);
+                frontLeft.setPower(0.5*leftPower + 0.5*extraPower);
+            }else if(leftPower < 0){
+                backLeft.setPower(0.5*leftPower - 0.5*extraPower);
+                frontLeft.setPower(0.5*leftPower - 0.5*extraPower);
+            }else{
+                backLeft.setPower(0);
+                frontLeft.setPower(0);
+            }
+
+            //In this case we add extraPower to the rightPower value
+            if(rightPower > 0){
+                frontRight.setPower(0.5*rightPower + 0.5*extraPower);
+                backRight.setPower(0.5*rightPower + 0.5*extraPower);
+            }else if(rightPower < 0){
+                frontRight.setPower(0.5*rightPower - 0.5*extraPower);
+                backRight.setPower(0.5*rightPower - 0.5*extraPower);
+            }else{
+                frontRight.setPower(0);
+                backRight.setPower(0);
+            }
+
             arm.setPower(armPower);
 
             telemetry.addData("Status", "Run Time: " + runtime.toString());
